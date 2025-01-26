d5a2a381ddb9c03aa59f6ecdb8a21c151182a559
Farhin Rahman <farhin.rahmanp@gmail.com>
2025-01-27 00:26:06 +0600

    fix: fix header alignment and functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,35 +1,77 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
-
+import { motion } from "framer-motion";
 export default function Header() {
   return (
-    <header>
-      <div className="">
+    <header className="sticky bg-teal-400 top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <div className="flex flex-row items-center">
+        <div className="relative group cursor-pointer">
+          <SocialIcon
+            url="https://www.linkedin.com/in/farhin-rahman-06510a27b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            fgColor="gray"
+            bgColor="transparent"
+            className="hover:bg-slate-900 rounded-e-3xl"
+          />
+          <span className="absolute left-1/2 -translate-x-1/2 bottom-[-20px] px-2 py-1 text-xs text-white bg-gray-400 opacity-0 group-hover:opacity-100 transition-opacity">
+            LinkedIn
+          </span>
+        </div>
+        <div className="relative group cursor-pointer">
+          <SocialIcon
+            url="https://www.facebook.com/farhin.rahman.169"
+            fgColor="gray"
+            bgColor="transparent"
+            className="hover:bg-slate-900 rounded-e-3xl"
+          />
+          <span className="absolute left-1/2 -translate-x-1/2 bottom-[-20px] px-2 py-1 text-xs text-white bg-gray-400 opacity-0 group-hover:opacity-100 transition-opacity">
+            Facebook
+          </span>
+        </div>
+        <div className="relative group cursor-pointer">
+          <SocialIcon
+            url="https://github.com/farhinrahman1"
+            fgColor="gray"
+            bgColor="transparent"
+            className="hover:bg-slate-900 rounded-e-3xl"
+          />
+          <span className="absolute left-1/2 -translate-x-1/2 bottom-[-20px] px-2 py-1 text-xs text-white bg-gray-400 opacity-0 group-hover:opacity-100 transition-opacity">
+            Github
+          </span>
+        </div>
+        <div className="relative group cursor-pointer">
+          <SocialIcon
+            url="https://x.com/farhinrahman_"
+            fgColor="gray"
+            bgColor="transparent"
+            className="hover:bg-slate-900 rounded-e-3xl"
+          />
+          <span className="absolute left-1/2 -translate-x-1/2 bottom-[-20px] px-2 py-1 text-xs text-white bg-gray-400 opacity-0 group-hover:opacity-100 transition-opacity">
+            X
+          </span>
+        </div>
+        <div className="relative group cursor-pointer">
+          <SocialIcon
+            url="https://www.instagram.com/f_a_e_f_i_n?igsh=MW5xbzF3bXpyNnBlaw=="
+            fgColor="gray"
+            bgColor="transparent"
+            className="hover:bg-slate-900 rounded-e-3xl"
+          />
+          <span className="absolute left-1/2 -translate-x-1/2 bottom-[-20px] px-2 py-1 text-xs text-white bg-gray-400 opacity-0 group-hover:opacity-100 transition-opacity">
+            Instagram
+          </span>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon
-          url="https://linkedin.com/in/couetilc"
+          url="https://email.com/in/couetilc"
           fgColor="gray"
           bgColor="transparent"
+          className="hover:bg-slate-900 rounded-s-3xl"
         />
-        <SocialIcon
-          url="https://facebook.com/in/couetilc"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://github.com/in/couetilc"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://twitter.com/in/couetilc"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://instagram.com/in/couetilc"
-          fgColor="gray"
-          bgColor="transparent"
-        />
+        <p className="font-semibold hidden md:inline-flex text-md text-gray-800">
+          Reach Out
+        </p>
       </div>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header";
+import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       {/* header */}
       <Header />
       {/* hero */}
+      <Hero />
       {/* about */}
       {/* experience */}
       {/* skills */}
