dbba0091233a3f59f3f898666431d3d5b7a99e12
Farhin Rahman <farhin.rahmanp@gmail.com>
2025-01-30 12:27:54 +0600

    fix: fix growth log section

--- components/About.tsx
+++ components/About.tsx
@@ -20,7 +20,7 @@ const skills = [
 export default function AboutPage() {
   const data = [
     {
-      title: "2024",
+      title: "Frontend Growth 2024",
       content: (
         <div>
           <p className="text-gray-300 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
@@ -60,16 +60,13 @@ export default function AboutPage() {
       ),
     },
     {
-      title: "Early 2023",
+      title: "2023 Frontend Log",
       content: (
         <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
-          </p>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Lorem ipsum is for people who are too lazy to write copy. But we are
-            not. Here are some more example of beautiful designs I built.
+          <p className="text-neutral-300 dark:text-neutral-200 text-sm md:text-sm font-normal mb-8">
+            I focused on frontend development, learning JavaScript, Next.js, and
+            deploying projects on GitHub. I also began exploring Figma for UI/UX
+            design.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
@@ -105,10 +102,10 @@ export default function AboutPage() {
       ),
     },
     {
-      title: "Changelog",
+      title: "Growth Log",
       content: (
         <div>
-          <p className="text-neutral-300 dark:text-neutral-200 text-sm md:text-sm font-normal mb-4">
+          <p className="text-neutral-300 dark:text-neutral-200 text-base md:text-sm font-normal mb-4">
             Completed several key milestones in my learning journey:
           </p>
           <div className="mb-8">
@@ -123,7 +120,7 @@ export default function AboutPage() {
                   className="w-4 h-4 accent-teal-500 bg-gray-100 border-gray-300 rounded-sm dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <span className="ms-2">
-                  Mastered HTML and CSS for building responsive and accessible
+                  Mastered HTML, CSS and JavaScript for building responsive
                   websites
                 </span>
               </div>
@@ -155,8 +152,8 @@ export default function AboutPage() {
                   className="w-4 h-4 accent-teal-500 bg-gray-100 border-gray-300 rounded-sm dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <span className="ms-2">
-                  Gained experience with Git for version control and
-                  collaborating on GitHub
+                  Experience with Git for version control, collaboration, and
+                  project deployment
                 </span>
               </div>
             </div>
@@ -167,21 +164,21 @@ export default function AboutPage() {
               alt="Amazon"
               width={600}
               height={300}
-              className="rounded object-contain h-36 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              className="object-contain h-36 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
               src={twitterImage}
               alt="Twitter"
               width={600}
               height={300}
-              className="rounded object-contain h-36 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              className="object-contain h-36 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
               src={githubImage}
               alt="Github Profile"
               width={400}
               height={300}
-              className=" object-contain h-24 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              className="object-contain h-24 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
           </div>
         </div>
