58c9faf9ffe7662158eb5981bda85a3f491f71e0
Farhin Rahman <farhin.rahmanp@gmail.com>
2025-05-10 19:48:13 +0600

    refine: skills journey error, quotes, colors

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -37,7 +37,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       className="w-full bg-black dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto py-10 px-4 md:px-8 lg:px-10">
+      <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-10">
         <h2 className="text-3xl font-bold mt-28 md:text-4xl mb-4 text-white dark:text-white max-w-4xl">
           Changelog from my journey
         </h2>
