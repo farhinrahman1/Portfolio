b5347dbdb4601962d0c9f516b9bb2380ec6db6b2
Farhin Rahman <farhin.rahmanp@gmail.com>
2025-06-22 21:12:38 +0600

    fix: hero section

--- components/shape-landing-hero.tsx
+++ components/shape-landing-hero.tsx
@@ -183,8 +183,8 @@ function HeroGeometric({
               Hi, I am a Frontend Developer. I bring designs to life with code.
             </p> */}
             <p className="text-center text-transparent bg-clip-text bg-gradient-to-r from-white/80 via-white/85 to-white/70 text-base sm:text-lg md:text-xl max-w-2xl mx-auto font-light tracking-wide animate-gradient">
-              Hi! I'm a{" "}
-              <span className="font-bold text-2xl bg-clip-text text-transparent bg-white">
+              I'm a{" "}
+              <span className="font-bold text-2xl bg-clip-text text-transparent bg-orange-200">
                 Frontend Developer.
               </span>{" "}
               Crafting smooth digital journeys, one interface at a time.
